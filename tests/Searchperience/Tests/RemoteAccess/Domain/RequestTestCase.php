0ba49f69d25503185fe8b13f315769275fbf239d
pavlo.bogomolenko <pavlo.bogomolenko@aoemedia.de>
2013-06-11 09:55:10 +0200

    Dummy Request model and test

--- tests/Searchperience/Tests/RemoteAccess/Domain/RequestTestCase.php
+++ tests/Searchperience/Tests/RemoteAccess/Domain/RequestTestCase.php
@@ -3,23 +3,35 @@
 
 namespace Searchperience\Tests\RemoteAccess\Domain;
 
+use Searchperience\RemoteAccess\Domain;
+
+
 /**
  * Class RequestTestCase
  * @package Searchperience\Tests\RemoteAccess\Domain
  */
 class RequestTestCase extends \Searchperience\Tests\BaseTestCase{
-
+    /**
+     * \Searchperience\RemoteAccess\Domain\Request $request
+     */
+    protected $request;
 	/**
 	 * @return void
 	 */
 	public function setUp() {
 		$this->setFixtureBasePath(__DIR__);
+        $this->request = new Domain\Request();
 	}
 
 	/**
 	 * @test
 	 */
 	public function testGetUrl() {
-
+        $this->request->setEndPointHostname("http://google.de/");
+        $this->request->setFacetOptionValue("brand_s", "puma");
+        $this->request->setFacetOptionValue("brand_s", "nike");
+        $this->request->setFacetOptionValue("cat_s", "cloth");
+        $url = $this->request->getUrl();
+        $this->assertEquals('http://google.de/index.php?id=0&dataType=jsonp&eID=tx_aoesolr_search&searchperience[facetsel][option][brand_s][0]=puma&searchperience[facetsel][option][brand_s][1]=nike&searchperience[facetsel][option][cat_s][0]=cloth', $url);
 	}
 }
