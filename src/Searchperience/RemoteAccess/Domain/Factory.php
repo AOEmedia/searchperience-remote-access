733407e80559f1b7b9c1f27eddc489e135724e91
Timo Schmidt <timo.schmidt@aoemedia.de>
2013-06-11 11:21:34 +0200

    Added Factory for outer usage

---
+++ src/Searchperience/RemoteAccess/Domain/Factory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Searchperience\RemoteAccess\Domain;
+
+/**
+ * Class Factory
+ *
+ * The factory class is responsible to create a client with all dependencies.
+ *
+ * @package Searchperience\RemoteAccess\Domain
+ */
+class Factory {
+	/**
+	 * @return \Searchperience\RemoteAccess\Domain\Client
+	 */
+	public static function createRemoteAccessClient() {
+		$searchperienceClient 		= new \Searchperience\RemoteAccess\Domain\Client();
+		$httpClient 				= new \Guzzle\Http\Client();
+		$searchperienceClient->injectHttpClient($httpClient);
+		return $searchperienceClient;
+	}
+
+	/**
+	 * @return \Searchperience\RemoteAccess\Domain\Request
+	 */
+	public static function createRequest() {
+		return new \Searchperience\RemoteAccess\Domain\Request();
+	}
+}
