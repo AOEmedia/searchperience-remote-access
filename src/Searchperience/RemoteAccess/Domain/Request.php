2aa0a2cf18f9e0b5b446ed7ba92b2067df97da0c
pavlo.bogomolenko <pavlo.bogomolenko@aoemedia.de>
2013-06-11 10:11:37 +0200

    fix indents

--- src/Searchperience/RemoteAccess/Domain/Request.php
+++ src/Searchperience/RemoteAccess/Domain/Request.php
@@ -11,215 +11,215 @@ namespace Searchperience\RemoteAccess\Domain;
  * @author Timo Schmidt <timo.schmidt@aoemedia.de>
  */
 class Request {
-    /**
-     * @var string
-     */
-    private $url;
+	/**
+	* @var string
+	*/
+	private $url;
 
-    /**
-     * @var string
-     */
-    private $endPointHostname = 'http://client.searchperience.me/';
+	/**
+	 * @var string
+	 */
+	private $endPointHostname = 'http://client.searchperience.me/';
 
-    /**
-     * @var string
-     */
-    private $endpointPath = 'index.php?id=###instance###';
+	/**
+	 * @var string
+	 */
+	private $endpointPath = 'index.php?id=###instance###';
 
-    /**
-     * @var string
-     */
-    private $instance = '0';
+	/**
+	 * @var string
+	 */
+	private $instance = '0';
 
-    /**
-     * @var string
-     */
-    private $namespace = 'searchperience';
+	/**
+	 * @var string
+	 */
+	private $namespace = 'searchperience';
 
-    /**
-     * @var array
-     */
-    private $facetOptionValue = array();
+	/**
+	 * @var array
+	 */
+	private $facetOptionValue = array();
 
-    /**
-     * @var string
-     */
-    private $dataType = 'jsonp';
+	/**
+	 * @var string
+	 */
+	private $dataType = 'jsonp';
 
-    /**
-     * @var string
-     */
-    private $eid = 'tx_aoesolr_search';
+	/**
+	 * @var string
+	 */
+	private $eid = 'tx_aoesolr_search';
 
-    const FACET_MARKER = 'searchperience[facetsel][option]';
+	const FACET_MARKER = 'searchperience[facetsel][option]';
 
-    /**
-     * @var array key/count
-     */
-    private $addedFacests = array();
+	/**
+	 * @var array key/count
+	 */
+	private $addedFacests = array();
 
-    /**
-     * @param string $eid
-     */
-    public function setEid($eid)
-    {
-        $this->eid = $eid;
-    }
+	/**
+	 * @param string $eid
+	 */
+	public function setEid($eid)
+	{
+		$this->eid = $eid;
+	}
 
-    /**
-     * @return string
-     */
-    public function getEid()
-    {
-        return $this->eid;
-    }
+	/**
+	 * @return string
+	 */
+	public function getEid()
+	{
+		return $this->eid;
+	}
 
-    /**
-     * @param string $dataType
-     */
-    public function setDataType($dataType)
-    {
-        $this->dataType = $dataType;
-    }
+	/**
+	 * @param string $dataType
+	 */
+	public function setDataType($dataType)
+	{
+		$this->dataType = $dataType;
+	}
 
-    /**
-     * @return string
-     */
-    public function getDataType()
-    {
-        return $this->dataType;
-    }
+	/**
+	 * @return string
+	 */
+	public function getDataType()
+	{
+		return $this->dataType;
+	}
 
-    /**
-     * @param string $endPointHostname
-     */
-    public function setEndPointHostname($endPointHostname)
-    {
-        $this->endPointHostname = $endPointHostname;
-        return $this;
-    }
+	/**
+	 * @param string $endPointHostname
+	 */
+	public function setEndPointHostname($endPointHostname)
+	{
+		$this->endPointHostname = $endPointHostname;
+		return $this;
+	}
 
-    /**
-     * @return string
-     */
-    public function getEndPointHostname()
-    {
-        return $this->endPointHostname;
-    }
+	/**
+	 * @return string
+	 */
+	public function getEndPointHostname()
+	{
+		return $this->endPointHostname;
+	}
 
-    /**
-     * @param string $endpointPath
-     */
-    public function setEndpointPath($endpointPath)
-    {
-        $this->endpointPath = $endpointPath;
-        return $this;
-    }
+	/**
+	 * @param string $endpointPath
+	 */
+	public function setEndpointPath($endpointPath)
+	{
+		$this->endpointPath = $endpointPath;
+		return $this;
+	}
 
-    /**
-     * @return string
-     */
-    public function getEndpointPath()
-    {
-        return $this->endpointPath;
-    }
+	/**
+	 * @return string
+	 */
+	public function getEndpointPath()
+	{
+		return $this->endpointPath;
+	}
 
-    /**
-     * @param $option
-     * @param $value
-     * @return $this
-     */
-    public function setFacetOptionValue($option, $value)
-    {
-        $this->facetOptionValue[] = array($option, $value);
-        return $this;
-    }
+	/**
+	 * @param $option
+	 * @param $value
+	 * @return $this
+	 */
+	public function setFacetOptionValue($option, $value)
+	{
+		$this->facetOptionValue[] = array($option, $value);
+		return $this;
+	}
 
-    /**
-     * @return array
-     */
-    public function getFacetOptionValue()
-    {
-        return $this->facetOptionValue;
-    }
+	/**
+	 * @return array
+	 */
+	public function getFacetOptionValue()
+	{
+		return $this->facetOptionValue;
+	}
 
-    /**
-     * @param int $instance
-     */
-    public function setInstance($instance)
-    {
-        $this->instancePid = $instance;
-        return this;
-    }
+	/**
+	 * @param int $instance
+	 */
+	public function setInstance($instance)
+	{
+		$this->instancePid = $instance;
+		return this;
+	}
 
-    /**
-     * @return int
-     */
-    public function getInstance()
-    {
-        return $this->instance;
-    }
+	/**
+	 * @return int
+	 */
+	public function getInstance()
+	{
+		return $this->instance;
+	}
 
-    /**
-     * @param string $namespace
-     */
-    public function setNamespace($namespace)
-    {
-        $this->namespace = $namespace;
-        return $this;
-    }
+	/**
+	 * @param string $namespace
+	 */
+	public function setNamespace($namespace)
+	{
+		$this->namespace = $namespace;
+		return $this;
+	}
 
-    /**
-     * @return string
-     */
-    public function getNamespace()
-    {
-        return $this->namespace;
-    }
+	/**
+	 * @return string
+	 */
+	public function getNamespace()
+	{
+		return $this->namespace;
+	}
 
-    /**
-     * @param string $url
-     */
-    public function setUrl($url)
-    {
-        $this->url = $url;
-        return $this;
-    }
+	/**
+	 * @param string $url
+	 */
+	public function setUrl($url)
+	{
+		$this->url = $url;
+		return $this;
+	}
 
-    /**
-     * @return string
-     */
-    private function generateFacetParams()
-    {
-        $facet = '';
-        $tmpFacetCount = 0;
-        $this->addedFacests = array();
-        foreach($this->getFacetOptionValue() as $facets){
-            if(array_key_exists($facets[0], $this->addedFacests)) {
-                $this->addedFacests[$facets[0]] = $this->addedFacests[$facets[0]] + 1;
-                $tmpFacetCount = $this->addedFacests[$facets[0]];
-            } else {
-                $this->addedFacests[$facets[0]] = 0;
-            }
-            $facet .= '&' . self::FACET_MARKER . '[' . $facets[0] .  ']' .
-                '[' . $tmpFacetCount  . ']' . '=' . $facets[1];
-            $tmpFacetCount = 0;
-        }
-        return $facet;
-    }
+	/**
+	 * @return string
+	 */
+	private function generateFacetParams()
+	{
+		$facet = '';
+		$tmpFacetCount = 0;
+		$this->addedFacests = array();
+		foreach($this->getFacetOptionValue() as $facets){
+			if(array_key_exists($facets[0], $this->addedFacests)) {
+				$this->addedFacests[$facets[0]] = $this->addedFacests[$facets[0]] + 1;
+				$tmpFacetCount = $this->addedFacests[$facets[0]];
+			} else {
+				$this->addedFacests[$facets[0]] = 0;
+			}
+			$facet .= '&' . self::FACET_MARKER . '[' . $facets[0] .  ']' .
+				'[' . $tmpFacetCount  . ']' . '=' . $facets[1];
+			$tmpFacetCount = 0;
+		}
+		return $facet;
+	}
 
-    /**
-     * @return string
-     */
-    public function getUrl()
-    {
-        $this->generateFacetParams();
-        $path = str_replace('###instance###', $this->getInstance(), $this->getEndpointPath());
-        $this->url = $this->getEndPointHostname()
-            . $path
-            . '&dataType='. $this->getDataType()
-            . '&eID=' . $this->getEid()
-            . $this->generateFacetParams();
-        return $this->url;
-    }
+	/**
+	 * @return string
+	 */
+	public function getUrl()
+	{
+		$this->generateFacetParams();
+		$path = str_replace('###instance###', $this->getInstance(), $this->getEndpointPath());
+		$this->url = $this->getEndPointHostname()
+			. $path
+			. '&dataType='. $this->getDataType()
+			. '&eID=' . $this->getEid()
+			. $this->generateFacetParams();
+		return $this->url;
+	}
 }
--- tests/Searchperience/Tests/RemoteAccess/Domain/RequestTestCase.php
+++ tests/Searchperience/Tests/RemoteAccess/Domain/RequestTestCase.php
@@ -21,18 +21,18 @@ class RequestTestCase extends \Searchperience\Tests\BaseTestCase {
 	 */
 	public function setUp() {
 		$this->setFixtureBasePath(__DIR__);
-        $this->request = new Domain\Request();
+		$this->request = new Domain\Request();
 	}
 
 	/**
 	 * @test
 	 */
 	public function testGetUrl() {
-        $testUrl = 'http://google.de/index.php?id=0&dataType=jsonp&eID=tx_aoesolr_search&searchperience[facetsel][option][brand_s][0]=puma&searchperience[facetsel][option][brand_s][1]=nike&searchperience[facetsel][option][cat_s][0]=cloth';
-        $this->request->setEndPointHostname('http://google.de/');
-        $this->request->setFacetOptionValue('brand_s', 'puma');
-        $this->request->setFacetOptionValue('brand_s', 'nike');
-        $this->request->setFacetOptionValue('cat_s', 'cloth');
-        $this->assertEquals($testUrl, $this->request->getUrl());
+		$testUrl = 'http://google.de/index.php?id=0&dataType=jsonp&eID=tx_aoesolr_search&searchperience[facetsel][option][brand_s][0]=puma&searchperience[facetsel][option][brand_s][1]=nike&searchperience[facetsel][option][cat_s][0]=cloth';
+		$this->request->setEndPointHostname('http://google.de/');
+		$this->request->setFacetOptionValue('brand_s', 'puma');
+		$this->request->setFacetOptionValue('brand_s', 'nike');
+		$this->request->setFacetOptionValue('cat_s', 'cloth');
+		$this->assertEquals($testUrl, $this->request->getUrl());
 	}
 }
